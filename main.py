e2ed08ba2f7d4187a9d51d80415933dee756c0ce
beans <ginahenstock7@gmail.com>
2021-06-15 13:28:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MyClient(discord.Client):
             #replace 'channelID' with the ID of the channel you want the bot to speak in
             channel = client.get_channel(channelID)
             fileObject = open("t.txt", "r")
+            #reads everything in file
             data = fileObject.read()
             await channel.send(data)
 
