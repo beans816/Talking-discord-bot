52812afee3aff8c8da6381fb6982ae251d873b8b
beans <ginahenstock7@gmail.com>
2021-06-15 13:15:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+#import discord token from abcd.py for security
+import abcd
+from abcd import token
+
+class MyClient(discord.Client):
+
+    async def on_ready(self)
+
+            #replace 'channelID' with the ID of the channel you want the bot to speak in
+            channel = client.get_channel(channelID)
+            fileObject = open("t.txt", "r")
+            data = fileObject.read()
+            await channel.send(data)
+
+client = MyClient()
+client.run(token)
